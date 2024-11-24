75d129c3006100b77205902e53150d947d5c839a
Holycrabeth <106599459+Holycrabeth@users.noreply.github.com>
2024-11-24 15:23:33 +0800

    Update arbitrage.sol

--- src/arbitrage.sol
+++ src/arbitrage.sol
@@ -19,18 +19,11 @@ interface ISushiSwapRouter {
 
 interface IWETH9 {
     function deposit() external payable;
-    function withdraw(uint256 wad) external;
-    function approve(address guy, uint256 wad) external returns (bool);
-    function transfer(address dst, uint256 wad) external returns (bool);
-    function transferFrom(address src, address dst, uint256 wad) external returns (bool);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address src) external view returns (uint256);
-    function allowance(address src, address guy) external view returns (uint256);
-
-    event Approval(address indexed src, address indexed guy, uint256 wad);
-    event Transfer(address indexed src, address indexed dst, uint256 wad);
-    event Deposit(address indexed dst, uint256 wad);
-    event Withdrawal(address indexed src, uint256 wad);
+    function withdraw(uint wad) external;
+    function approve(address dst, uint wad) external returns (bool);
+    function transfer(address dst, uint wad) external returns (bool);
+    function transferFrom(address src, address dst, uint wad) external returns (bool);
+    function balanceOf(address owner) external view returns (uint);
 }
 
 contract MyContract {
