95b380bd66a0daf0d482e080823fb4333fc8eee0
Holycrabeth <106599459+Holycrabeth@users.noreply.github.com>
2024-11-29 10:36:29 +0800

    Update sampleDetection.js

--- src/detectArbitrage/sampleDetection.js
+++ src/detectArbitrage/sampleDetection.js
@@ -1,51 +1,58 @@
-const ethers = require('ethers');
-const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/abc');
-const token0 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH
-const token1 = '0x6b175474e89094c44da98b954eedeac495271d0f'; // DAI
-const uniRouterAddress = '0x7a250d5630b4cf539739df2c5dacb4c659f2488d';
-const sushiRouterAddress = '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F';
-const PATH = [token0, token1];
-const TX_FEE = 0.003;  // Approximate transaction fee as a percentage
+// Importing Hardhat's ethers
+const { ethers } = require("hardhat");
 
-const routerAbi = [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-];
+async function main() {
+    const provider = ethers.provider;  // Using Hardhat's default provider
+    const signers = await ethers.getSigners();  // Get the list of accounts from Hardhat
+    
+    const token0 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'; // WETH
+    const token1 = '0x6b175474e89094c44da98b954eedeac495271d0f'; // DAI
+    const uniRouterAddress = '0x7a250d5630b4cf539739df2c5dacb4c659f2488d';
+    const sushiRouterAddress = '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F';
+    const PATH = [token0, token1];
+    const TX_FEE = 0.003;  // Transaction fee percentage
 
-const uniRouter = new ethers.Contract(uniRouterAddress, routerAbi, provider);
-const sushiRouter = new ethers.Contract(sushiRouterAddress, routerAbi, provider);
-const amountIn = ethers.utils.parseEther('1');
+    const routerAbi = [
+        'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
+    ];
 
-async function checkArbitrage() {
-  const amountsOutUni = await uniRouter.getAmountsOut(amountIn, PATH);
-  const amountsOutSushi = await sushiRouter.getAmountsOut(amountIn, PATH);
+    const uniRouter = new ethers.Contract(uniRouterAddress, routerAbi, signers[0]);
+    const sushiRouter = new ethers.Contract(sushiRouterAddress, routerAbi, signers[0]);
+    const amountIn = ethers.utils.parseEther('1');
 
-  const uniPrice = amountsOutUni[1];
-  const sushiPrice = amountsOutSushi[1];
+    const amountsOutUni = await uniRouter.getAmountsOut(amountIn, PATH);
+    const amountsOutSushi = await sushiRouter.getAmountsOut(amountIn, PATH);
 
-  // Effective price after considering transaction fees
-  const effectiveUniPrice = uniPrice.add(uniPrice.mul(TX_FEE));
-  const effectiveSushiPrice = sushiPrice.add(sushiPrice.mul(TX_FEE));
+    const uniPrice = amountsOutUni[1];
+    const sushiPrice = amountsOutSushi[1];
 
-  // Calculate the spread between Uniswap and Sushiswap
-  if (uniPrice.gt(sushiPrice)) {
-    const spread = effectiveUniPrice.sub(effectiveSushiPrice);
-    console.log('Uni to Sushi spread:', spread.toString());
-    if (spread.gt(0)) {
-      console.log('Sell on Uni, buy on Sushi');
+    const effectiveUniPrice = uniPrice.add(uniPrice.mul(TX_FEE));
+    const effectiveSushiPrice = sushiPrice.add(sushiPrice.mul(TX_FEE));
+
+    if (uniPrice.gt(sushiPrice)) {
+        const spread = effectiveUniPrice.sub(effectiveSushiPrice);
+        console.log('Uni to Sushi spread:', spread.toString());
+        if (spread.gt(0)) {
+            console.log('Sell on Uni, buy on Sushi');
+        } else {
+            console.log('No arbitrage opportunity');
+        }
+    } else if (sushiPrice.gt(uniPrice)) {
+        const spread = effectiveSushiPrice.sub(effectiveUniPrice);
+        console.log('Sushi to Uni spread:', spread.toString());
+        if (spread.gt(0)) {
+            console.log('Sell on Sushi, buy on Uni');
+        } else {
+            console.log('No arbitrage opportunity');
+        }
     } else {
-      console.log('No arbitrage opportunity');
+        console.log('No arbitrage opportunity');
     }
-  } else if (sushiPrice.gt(uniPrice)) {
-    const spread = effectiveSushiPrice.sub(effectiveUniPrice);
-    console.log('Sushi to Uni spread:', spread.toString());
-    if (spread.gt(0)) {
-      console.log('Sell on Sushi, buy on Uni');
-    } else {
-      console.log('No arbitrage opportunity');
-    }
-  } else {
-    console.log('No arbitrage opportunity');
-  }
 }
 
-checkArbitrage();
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
